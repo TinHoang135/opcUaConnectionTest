01fb6d0bede85ef2a27084bd0523311dafca5681
Hoang, Tin <hoang.t@pg.com>
2026-05-15 11:45:55 +0200

    Commit

--- opcUaConnectionTest/OPC/OpcUaTester.cs
+++ opcUaConnectionTest/OPC/OpcUaTester.cs
@@ -79,11 +79,11 @@ namespace opcUaConnectionTest.OPC
             }
 
             await OpcUaConnectionTestWallE(cancellationToken);
-            // await ExecuteWallECobotProgram(cancellationToken);
-            await OpcUaConnectionTestEVE(cancellationToken);
+            await ExecuteWallECobotProgram(cancellationToken);
+            // await OpcUaConnectionTestEVE(cancellationToken);
             // await ExecuteFanucCobotProgram(cancellationToken);
             // await ExecuteTowerProgram(cancellationToken);   
-            await ExecuteSafetyZoneMuteProgram(cancellationToken);
+            // await ExecuteSafetyZoneMuteProgram(cancellationToken);
         }
 
         private static OpcUaApplication LoadOpcUaApplication()
